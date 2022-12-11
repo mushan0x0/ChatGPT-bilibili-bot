1755580ad86b3e6ac4fa6d226d8de97d75153e7e
liaozijian <lzj960515@163.com>
2022-12-11 12:34:57 +0800

    更换获取地址方式

--- src/bilibili-bot.ts
+++ src/bilibili-bot.ts
@@ -1,20 +1,23 @@
-import puppeteer, {Page} from "puppeteer";
-
-// 读取并且回复评论
-const videoUrl =
-    "https://www.bilibili.com/video/BV13P4y117S2/?vd_source=94d46242b5004349ffbc65d6cb74bc57";
+import puppeteer, { Page } from "puppeteer";
 
 export default class Bot {
   private page;
   private browser;
-  scan?: ((message: string) => void);
+  scan?: (message: string) => void;
   login: ((message: string) => void) | undefined;
-  logout?: ((message: string) => void);
-  message?: ((message: string) => Promise<string>);
-  friendship?: ((message: string) => void);
+  logout?: (message: string) => void;
+  message?: (message: string) => Promise<string>;
+  friendship?: (message: string) => void;
+  getVideoUrl?: (message: string) => void;
   on(
-      event: "scan" | "login" | "logout" | "message" | "friendship",
-      callback: (message: string) => void
+    event:
+      | "scan"
+      | "login"
+      | "logout"
+      | "message"
+      | "friendship"
+      | "getVideoUrl",
+    callback: (message: string) => void
   ) {
     // @ts-ignore
     this[event] = callback;
@@ -36,12 +39,15 @@ export default class Bot {
 
     try {
       // 如果有头像元素了代表已经登录了
-      const avatarEle = await page.waitForSelector('.header-avatar-wrap--container', { timeout: 5000 }) as any;
+      const avatarEle = (await page.waitForSelector(
+        ".header-avatar-wrap--container",
+        { timeout: 5000 }
+      )) as any;
       // TODO: 把用户名传给登录回调
-      this.login?.('用户名xxx');
+      this.login?.("用户名xxx");
     } catch (error) {
       // 显示二维码登录
-      await this.handleScan()
+      await this.handleScan();
     }
 
     // 遍历评论并回复
@@ -55,52 +61,61 @@ export default class Bot {
   eachComments = async () => {
     const page = this.page;
     // 跳转到视频详情页面
+    let videoUrl = this.getVideoUrl?.("xx");
     await page.goto(videoUrl);
     await page.waitForSelector(".reply-item", { timeout: 99999999 });
     // 获取到待回复的评论
     const replyItems = await page.evaluate(() => {
-      return Array.from(document.querySelectorAll('.reply-item'))
-        .map((el, index) => {
-          const rootItem: any = el.querySelectorAll('.reply-content')[0];
+      return Array.from(document.querySelectorAll(".reply-item")).map(
+        (el, index) => {
+          const rootItem: any = el.querySelectorAll(".reply-content")[0];
           const rootComment = rootItem.innerText;
           // 给按钮设置一个id，方便后面需要回复时查找
-          const rootReplayBtnId = 'btn' + index;
-          const rootReplayBtn: any = el.querySelector('.root-reply-container .reply-btn');
+          const rootReplayBtnId = "btn" + index;
+          const rootReplayBtn: any = el.querySelector(
+            ".root-reply-container .reply-btn"
+          );
           rootReplayBtn.id = rootReplayBtnId;
-          const allComments = Array.from(el.querySelectorAll('.reply-content'))
-            .map(el => el.textContent)
+          const allComments = Array.from(
+            el.querySelectorAll(".reply-content")
+          ).map((el) => el.textContent);
           // 是否有折叠评论
-          const hasMore = el.querySelector('.view-more-default')
+          const hasMore = el.querySelector(".view-more-default");
           // 回复里包含来自自动回复的表示已经回复过了
-          const isReplied = allComments.some((comment) => comment?.includes('来自自动回复：')) || hasMore;
+          const isReplied =
+            allComments.some((comment) =>
+              comment?.includes("来自自动回复：")
+            ) || hasMore;
           return {
             rootComment,
             isReplied,
             rootReplayBtnId,
-          }
-        });
-
+          };
+        }
+      );
     });
-    console.log(`有${replyItems.filter(({isReplied}) => !isReplied).length}条待回复评论`)
+    console.log(
+      `有${replyItems.filter(({ isReplied }) => !isReplied).length}条待回复评论`
+    );
     // 遍历评论并回复
-    for (const {rootComment, isReplied, rootReplayBtnId} of replyItems) {
+    for (const { rootComment, isReplied, rootReplayBtnId } of replyItems) {
       if (!isReplied) {
         // 点击回复按钮
         (await page.$(`#${rootReplayBtnId}`)).click();
         // 没有回复过的话就获取自动回复
         const result = `来自自动回复：${await this.message?.(rootComment)}`;
         // 输入回复
-        (await page.$('.reply-item .reply-box-textarea')).type(result);
+        (await page.$(".reply-item .reply-box-textarea")).type(result);
         // 等待输入完成
-        await new Promise(resolve => setTimeout(resolve, 2000));
+        await new Promise((resolve) => setTimeout(resolve, 2000));
         // 点击发送
-        (await page.$('.reply-item .send-text')).click();
+        (await page.$(".reply-item .send-text")).click();
       }
     }
-    console.log('回复完成，等待5秒后刷新')
+    console.log("回复完成，等待5秒后刷新");
     // 回复完成后等待5秒刷新页面再回复
     setTimeout(this.eachComments, 5000);
-  }
+  };
   async start() {
     await this.init();
   }
@@ -108,18 +123,18 @@ export default class Bot {
     const page = this.page;
     // 点击登录按钮
     (
-        await page.waitForSelector(".go-login-btn", { timeout: 99999999 })
+      await page.waitForSelector(".go-login-btn", { timeout: 99999999 })
     )?.click();
 
     // 等待页面中出现登录二维码
     await page.waitForSelector(
-        '[title^="https://passport.bilibili.com/h5-app/passport/login/scan"]',
-        { timeout: 99999999 }
+      '[title^="https://passport.bilibili.com/h5-app/passport/login/scan"]',
+      { timeout: 99999999 }
     );
     const qr = await page.evaluate(() => {
       // 在页面中查找title属性以https://passport.bilibili.com/h5-app/passport/login/scan开头的标签
       const element = document.querySelector(
-          '[title^="https://passport.bilibili.com/h5-app/passport/login/scan"]'
+        '[title^="https://passport.bilibili.com/h5-app/passport/login/scan"]'
       ) as any;
 
       return element.getAttribute("title");
@@ -129,6 +144,8 @@ export default class Bot {
     // @ts-ignore
     this.scan?.(qr);
     // 等待扫码成功
-    await page.waitForSelector('.header-avatar-wrap--container', { timeout: 99999999 })
+    await page.waitForSelector(".header-avatar-wrap--container", {
+      timeout: 99999999,
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,59 @@
-import { ChatGPTAPI } from 'chatgpt'
-import pTimeout from 'p-timeout'
-import qrcodeTerminal from 'qrcode-terminal'
-import Bot from './bilibili-bot'
-import fs from 'fs';
-import readline from 'readline'
+import { ChatGPTAPI } from "chatgpt";
+import pTimeout from "p-timeout";
+import qrcodeTerminal from "qrcode-terminal";
+import Bot from "./bilibili-bot";
+import fs from "fs";
+import readline from "readline";
 
 const config = {
   AutoReply: true,
   MakeFriend: true,
-  ChatGPTSessionToken: ''
-}
+  ChatGPTSessionToken: "",
+};
 
 async function getChatGPTReply(content) {
-  const api = new ChatGPTAPI({ sessionToken: config.ChatGPTSessionToken })
+  const api = new ChatGPTAPI({ sessionToken: config.ChatGPTSessionToken });
   // ensure the API is properly authenticated (optional)
-  await api.ensureAuth()
+  await api.ensureAuth();
   // send a message and wait for the response
   //TODO: format response to compatible with wechat messages
-  const threeMinutesMs = 10 * 60 * 1000
-  const response = await pTimeout(
-    api.sendMessage(content),
-    {
-      milliseconds: threeMinutesMs,
-      message: 'ChatGPT timed out waiting for response'
-    }
-  )
-  console.log('response: ', response);
+  const threeMinutesMs = 10 * 60 * 1000;
+  const response = await pTimeout(api.sendMessage(content), {
+    milliseconds: threeMinutesMs,
+    message: "ChatGPT timed out waiting for response",
+  });
+  console.log("response: ", response);
   // response is a markdown-formatted string
-  return response
+  return response;
 }
 
-
 async function onMessage(msg) {
-  console.log('content: ', msg);
+  console.log("content: ", msg);
   let result;
   try {
-    result = await getChatGPTReply(msg)
+    result = await getChatGPTReply(msg);
   } catch (e: any) {
     console.log(e.message);
-    console.log('获取超时，重试中...')
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("获取超时，重试中...");
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     return onMessage(msg);
   }
   return result;
 }
 
-
 function onScan(qrcode) {
   qrcodeTerminal.generate(qrcode); // 在console端显示二维码
   const qrcodeImageUrl = [
-    'https://api.qrserver.com/v1/create-qr-code/?data=',
+    "https://api.qrserver.com/v1/create-qr-code/?data=",
     encodeURIComponent(qrcode),
-  ].join('');
+  ].join("");
 
-  console.log('Please open bilibili and scan the code to log in.');
+  console.log("Please open bilibili and scan the code to log in.");
 }
 
 async function onLogin(user) {
   console.log(`${user} has logged in`);
-  const date = new Date()
+  const date = new Date();
   console.log(`Current time:${date}`);
   if (config.AutoReply) {
     console.log(`Automatic robot chat mode has been activated`);
@@ -67,6 +62,11 @@ async function onLogin(user) {
 
 function onLogout(user) {
   console.log(`${user} has logged out`);
+}
+
+async function onGetVideoUrl(url) {
+  let videoUrl = await getVideoUrl();
+  return videoUrl;
 }
 // async function onFriendShip(friendship) {
 //   const frienddShipRe = /chatgpt|chat/
@@ -77,29 +77,38 @@ function onLogout(user) {
 //   }
 // }
 
-
-
 const bot = new Bot();
 
-bot.on('scan', onScan);
-bot.on('login', onLogin);
-bot.on('logout', onLogout);
-bot.on('message', onMessage)
+bot.on("scan", onScan);
+bot.on("login", onLogin);
+bot.on("logout", onLogout);
+bot.on("message", onMessage);
+bot.on("getVideoUrl", onGetVideoUrl);
 if (config.MakeFriend) {
   // bot.on('friendship', onFriendShip);
 }
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 // 定义一个函数用于提示用户输入登录 token
 function promptForToken() {
   return new Promise((resolve, reject) => {
-    rl.question('Please enter your ChatGPT session token: ', (token) => {
+    rl.question("Please enter your ChatGPT session token: ", (token) => {
       rl.close();
       resolve(token);
+    });
+  });
+}
+
+// 定义一个函数用于提示用户输入视频地址
+function promptForVideoUrl() {
+  return new Promise((resolve, reject) => {
+    rl.question("Please enter your bilibili video url: ", (videoUrl) => {
+      rl.close();
+      resolve(videoUrl);
     });
   });
 }
@@ -107,7 +116,7 @@ function promptForToken() {
 // 定义一个函数用于尝试从文件中读取登录 token
 function readTokenFromFile() {
   try {
-    return fs.readFileSync('token.txt', 'utf8');
+    return fs.readFileSync("token.txt", "utf8");
   } catch (err) {
     return null;
   }
@@ -117,10 +126,16 @@ function readTokenFromFile() {
 async function getToken() {
   let token = readTokenFromFile();
   if (!token) {
-    token = await promptForToken() as string;
-    fs.writeFileSync('token.txt', token);
+    token = (await promptForToken()) as string;
+    fs.writeFileSync("token.txt", token);
   }
   return token as string;
+}
+
+// 定义一个函数用于尝试从文件(TODO：读取视频列表)中读取视频地址，如果读取失败就提示用户输入
+async function getVideoUrl() {
+  let videoUrl = (await promptForVideoUrl()) as string;
+  return videoUrl as string;
 }
 
 // 现在您可以调用 getToken 函数来获取登录 token，然后使用它登录您的应用
@@ -128,6 +143,6 @@ getToken().then((token) => {
   config.ChatGPTSessionToken = token;
   bot
     .start()
-    .then(() => console.log('Start to log in bilibili...'))
+    .then(() => console.log("Start to log in bilibili..."))
     .catch((e) => console.error(e));
 });
